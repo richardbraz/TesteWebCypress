c9416275844f447056b5b5d50504ebaf17673d7f
richardbraz <richardbraz@outlook.com>
2023-02-17 13:04:12 -0300

    Alteração na limpeza de cookies

--- cypress/e2e/AutomacaoFront/compra.com.sucesso.cy.js
+++ cypress/e2e/AutomacaoFront/compra.com.sucesso.cy.js
@@ -6,7 +6,7 @@ describe('Testes de compra', () => {
 
 //Teste de compra com sucesso
          
-cy.clearCookies
+cy.clearAllCookies()
         cy.visit('https://www.saucedemo.com/')
         cy.get('[data-test="username"]').click().type('performance_glitch_user')
         cy.wait(1500)
